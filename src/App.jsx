aa468370fc451eb2f81cf0e59b9978243ac801e4
Radhika <radhika@whileone.in>
2024-02-19 17:04:09 +0530

    button added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import { useEffect } from "react";
 function App() {
   const [value, setValue] = useState(0);
 
+  function restartProgressBar () {
+    setValue(0);
+  }
   useEffect(() => {
     setInterval(() => {
       setValue((val) => val + 1);
@@ -17,6 +20,7 @@ function App() {
       <div className="app">
         <span>Progress Bar</span>
         <ProgressBar value={value} />
+        <button onClick={restartProgressBar}>Restart</button>
       </div>
     </>
   );
